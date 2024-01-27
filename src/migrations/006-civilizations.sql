d78dcffe663cb5c40d128ed47e3e6531d8a96cfe
Stanislas Daniel Claude Dolcini <stanislas.dolcini@gmail.com>
2024-01-27 16:50:29 +0100

    feat: add civ table refs #2
    Signed-off-by: Stanislas Daniel Claude Dolcini <stanislas.dolcini@gmail.com>

---
+++ src/migrations/006-civilizations.sql
@@ -0,0 +1,34 @@
+
+CREATE TABLE civilizations (
+  "id" integer NOT NULL CONSTRAINT "PK_replay_lobby_player_link" PRIMARY KEY,
+  "key" text NOT NULL,
+  "emblem_key" text NOT NULL,
+  "modification_date" timestamp
+  with
+    time zone NULL DEFAULT (CURRENT_TIMESTAMP),
+  "creation_date" timestamp
+  with
+    time zone NULL DEFAULT (CURRENT_TIMESTAMP),
+);
+
+Insert Into civilizations ("key", "emblem_key") Values
+('ptol', 'ptolemies'),
+('cart', 'carthaginians'),
+('han', 'han'),
+('spart', 'spartans'),
+('mace', 'macedonians'),
+('rome', 'romans'),
+('athen', 'athenians'),
+('maur', 'mauryas'),
+('sele', 'seleucids'),
+('gaul', 'celts'),
+('brit', 'celts'),
+('iber', 'iberians'),
+('kush', 'kushites'),
+('pers', 'persians');
+
+--------------------------------------------------------------------------------
+-- Down
+--------------------------------------------------------------------------------
+
+DROP TABLE civilizations;
